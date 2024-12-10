00ba1d6427651ed35ec2d58cd1cb166ddbd150dc
Tiffany Zhang <tzhang@lcs.on.ca>
2024-12-10 11:37:50 -0500

    Initial Commit

---
+++ PockerApp/PockerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PockerAppApp.swift
+//  PockerApp
+//
+//  Created by xinyu zhang on 2024-12-10.
+//
+
+import SwiftUI
+
+@main
+struct PockerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
